c3643522a1f152c92d60ca9d4b1a08edfce52b2e
Tim Perry <pimterry@gmail.com>
2017-10-20 15:19:05 +0100

    Scrolling spinner

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Go!');
+const Blinkt = require('node-blinkt');
+
+const leds = new Blinkt();
+leds.clearAll();
+leds.sendUpdate();
+
+console.log('Starting');
+
+let currentLed = 0;
+setInterval(() => {
+    console.log('Set pixel', currentLed);
+    leds.clearAll();
+    leds.setPixel(currentLed, 255, 255, 255, 1);
+    leds.sendUpdate();
+
+    currentLed = (currentLed + 1) % 8;
+}, 1000);
