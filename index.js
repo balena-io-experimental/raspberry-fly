f118c417ec55a5ecd31d776cdbb478029deeb596
Tim Perry <pimterry@gmail.com>
2017-10-21 17:00:29 +0200

    Drop everything

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const Blinkt = require('node-blinkt');
-
-const leds = new Blinkt();
-leds.setup();
-leds.clearAll();
-leds.sendUpdate();
-
-console.log('Starting');
-
-let currentLed = 0;
 setInterval(() => {
-    console.log('Set pixel', currentLed);
-    leds.clearAll();
-    leds.setPixel(currentLed, 255, 255, 255, 1);
-    leds.sendUpdate();
-
-    currentLed = (currentLed + 1) % 8;
+  console.log('Still alive');
 }, 1000);
