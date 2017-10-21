51c0655e43e8e0bc3082bc9d84308b54932e79b8
Tim Perry <pimterry@gmail.com>
2017-10-21 17:13:19 +0200

    Zooming pixel

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const ws281x = require('rpi-ws281x-native');
+const _ = require('lodash');
+
+ws281x.init(32);
+
+const zeros = [
+  0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0
+];
+
+ws281x.render(zeros);
+
+let pixelIndex = 0;
 setInterval(() => {
   console.log('Still alive');
-}, 1000);
+
+  let pixels = _.clone(zeros);
+  pixels[pixelIndex] = 0xaa0000;
+  pixelIndex = (pixelIndex + 1) % 32;
+  ws281x.render(pixels);
+}, 100);
